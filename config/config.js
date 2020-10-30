4221a4bab38e5ac2476da1e1f9b5ab95067ddb68
EC2 Default User <ec2-user@ip-172-31-5-102.us-east-2.compute.internal>
2020-10-30 00:56:05 +0000

    Fix Config

--- config/config.js
+++ config/config.js
@@ -8,26 +8,17 @@ module.exports = {
     storage: ":memory:"
   },
   dev: {
-    username: "manifestUser",
-    password: "manifestPassword",
-    database: "blogapp",
-    host: "manifestdbinstance.cgq0reqixqsd.us-east-1.rds.amazonaws.com",
+    username: "nationscash",
+    password: "1456Ee241",
+    database: "ebookstore",
+    host: "starlight.cvtudh5ojaen.us-east-2.rds.amazonaws.com",
     dialect: 'postgres' 
   },
   production: {
-    username: "manifestUser",
-    password: "manifestPassword",
-    database: "blogapp",
-    host: "manifestdbinstance.cgq0reqixqsd.us-east-1.rds.amazonaws.com",
+    username: "nationscash",
+    password: "1456Ee241",
+    database: "ebookstore",
+    host: "starlight.cvtudh5ojaen.us-east-2.rds.amazonaws.com",
     dialect: 'postgres' 
   }
-  // ,
-  // production: {
-  //   username: process.env.DB_USERNAME,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_NAME,
-  //   host: process.env.DB_HOSTNAME,
-  //   dialect: 'mysql',
-  //   use_env_variable: 'DATABASE_URL'
-  // }
 };
